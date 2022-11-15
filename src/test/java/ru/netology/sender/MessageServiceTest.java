6ba46304c324a3da1711ea78267950de96e74279
varlamov.ks <varlamov.ks@mail.ru>
2022-11-15 23:15:40 +0300

    Исправление замечаний проверяющего

--- src/test/java/ru/netology/sender/MessageServiceTest.java
+++ src/test/java/ru/netology/sender/MessageServiceTest.java
@@ -1,6 +1,8 @@
 package ru.netology.sender;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mockito;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
@@ -16,33 +18,36 @@ public class MessageServiceTest {
     public static final String LOCALHOST = "127.0.0.1";
     public static final String MOSCOW_IP = "172.0.32.11";
     public static final String NEW_YORK_IP = "96.44.183.149";
-    private final Location RU_LOCATION = new Location("Moscow", Country.RUSSIA, "def street", 1);
-    private final Location USA_LOCATION = new Location("New York", Country.USA, "def street", 1);
+    public static final GeoServiceImpl GEO_SERVICE = Mockito.mock(GeoServiceImpl.class);
+    public static final LocalizationService LOCALIZATION_SERVICE = Mockito.mock(LocalizationService.class);
+    public static final MessageSender MESSAGE_SENDER = new MessageSenderImpl(GEO_SERVICE, LOCALIZATION_SERVICE);
+    private static final Location RU_LOCATION = new Location("Moscow", Country.RUSSIA, "def street", 1);
+    private static final Location USA_LOCATION = new Location("New York", Country.USA, "def street", 1);
 
-    @Test
-    public void testSMessageSenderImpl() {
-        GeoServiceImpl geoService = Mockito.mock(GeoServiceImpl.class);
-        Mockito.when(geoService.byIp(MOSCOW_IP)).thenReturn(RU_LOCATION);
-        Mockito.when(geoService.byIp(NEW_YORK_IP)).thenReturn(USA_LOCATION);
-        Mockito.when(geoService.byIp(LOCALHOST)).thenReturn(USA_LOCATION);
-
-        LocalizationService localizationService = Mockito.mock(LocalizationService.class);
-        Mockito.when(localizationService.locale(Country.RUSSIA)).thenReturn("Добро пожаловать");
-        Mockito.when(localizationService.locale(Country.USA)).thenReturn("Welcome");
-
-        MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
-
-        Map<String, String> headers = new HashMap<>();
-        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, MOSCOW_IP);
-        assertEquals(messageSender.send(headers), "Добро пожаловать");
-
-        headers = new HashMap<>();
-        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, NEW_YORK_IP);
-        assertEquals(messageSender.send(headers), "Welcome");
-
-        headers = new HashMap<>();
-        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, LOCALHOST);
-        assertEquals(messageSender.send(headers), "Welcome");
+    @BeforeAll
+    public static void mockAll() {
+        Mockito.when(GEO_SERVICE.byIp(MOSCOW_IP)).thenReturn(RU_LOCATION);
+        Mockito.when(GEO_SERVICE.byIp("172.10.10.1")).thenReturn(RU_LOCATION);
+        Mockito.when(GEO_SERVICE.byIp(NEW_YORK_IP)).thenReturn(USA_LOCATION);
+        Mockito.when(GEO_SERVICE.byIp("92.20.10.11")).thenReturn(USA_LOCATION);
+        Mockito.when(GEO_SERVICE.byIp(LOCALHOST)).thenReturn(USA_LOCATION);
+        Mockito.when(LOCALIZATION_SERVICE.locale(Country.RUSSIA)).thenReturn("Добро пожаловать");
+        Mockito.when(LOCALIZATION_SERVICE.locale(Country.USA)).thenReturn("Welcome");
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {MOSCOW_IP, "172.10.10.1"})
+    public void sendRightRusIP(String ip) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, ip);
+        assertEquals(MESSAGE_SENDER.send(headers), "Добро пожаловать");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {NEW_YORK_IP, "92.20.10.11", LOCALHOST})
+    public void sendNotRusIP(String ip) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, ip);
+        assertEquals(MESSAGE_SENDER.send(headers), "Welcome");
+    }
 }
